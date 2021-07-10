b1c9846144c3b54c4ee7bc9b64f01d9acc271161
tana-16 <tanahiro1697@gmail.com>
2021-07-10 07:52:43 +0000

    [Finish]3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,11 @@
 class BlogsController < ApplicationController
+  def index
+    # 記事を全件取得
+    @blogs = Blog.all
+  end
 
   def show
     @blog = Blog.find(params[:id])
-  end
-
-  def index
-    @blogs = Blog.all
   end
 
   def new
@@ -15,7 +15,7 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blog_path(blog.id)
+    redirect_to blog_path(blog.id)  #この行を修正
   end
 
   def edit
@@ -28,9 +28,15 @@ class BlogsController < ApplicationController
     redirect_to blog_path(blog)
   end
 
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
-
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,4 +20,11 @@ ActiveRecord::Schema.define(version: 2021_07_08_075223) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
